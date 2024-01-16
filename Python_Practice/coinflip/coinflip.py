1b6b4c6079499fd7f797d72a81e398383e3f865d
jeffreychee <jeffreychee86@gmail.com>
2024-01-16 01:04:05 -0500

    tracked outcomes and percentages of results

--- Python_Practice/coinflip/coinflip.py
+++ Python_Practice/coinflip/coinflip.py
@@ -5,7 +5,7 @@
 # argparse library to create a more user-friendly interface, 
 # as well as computing statistical results. 
 
-# Basic Coin Flip Simulation with Random Number Generation
+# 1. Basic Coin Flip Simulation with Random Number Generation
 import random # like JS this is a module that allows us to generate random numbers like JS's Math.random()
 
 print(random.randint(0,1)) # randint is a method that generates a random integer between the two arguments passed to it
@@ -17,8 +17,12 @@ print(random.randint(0,1)) # randint is a method that generates a random integer
 # one of these numbers at random, and print outputs it to 
 # the console.
 
+#-----------------------------------------------------------------------
 
+# 2. More Detail
 # Coin Flip Simulation with Command-Line Arguments
+# Using the Ternal Operator and printing out the 
+# result of each coin flip
 import random
 
 for i in range(10):
@@ -30,3 +34,26 @@ for i in range(10):
     # f-string is used for string formatting. It embeds expressions inside string literals.
     # {i+1} is used to print the flip number (starting from 1 instead of 0)
     # {face} prints 'H' for heads or 'T' for tails
+
+#-----------------------------------------------------------------------
+
+# 3. Track outcomes and calculate percentages with a fixed number of flips
+# and print the results of the coin flips
+import random
+
+NUM_FLIPS = 10 # defining the number of flips as a constant
+
+total = 0 # initializing a var to keep track of the total number of heads
+for i in range(NUM_FLIPS):
+    flip = random.randint(0,1)
+    total += flip # add to flip count if flip is 1 (heads)
+    face = 'H' if flip else 'T' 
+    print(f"Flip #{i+1}: {face}") # f-string for formating like JS (`${expression}`)
+
+print(f"Result: {total} Heads, {NUM_FLIPS - total} Tails. "
+      f"({total / NUM_FLIPS * 100:.2f}% heads)")
+# Print the final results
+# Calculate and print the number of heads, tails, and the percentage of heads
+# {:.2f} in f-string is for formatting to 2 decimal places, akin to toFixed(2) in JS
+
+#-----------------------------------------------------------------------
