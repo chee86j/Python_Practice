11d505ccde5bcc1fa88c40099f2257e79db8c7ac
chee86j <jeffreychee86@gmail.com>
2024-01-25 00:27:05 -0500

    dice base solution added

---
+++ Python_Practice/dice/dice.py
@@ -0,0 +1,10 @@
+# Basic Dice Rolling Simulator Solution
+import random # import random module
+
+num_rolls = 3 # set variable for number of rolls
+
+die1 = random.randint(1, 6) # set function for die1 using random module and randint function
+die2 = random.randint(1, 6) # set function for die2 using random module and randint function
+print(die1, die2) # print results of die1 and die2
+
+# -------------------------------------------------------------------------------------------
