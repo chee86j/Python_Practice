e48b6742af84ebba73f6eef169a5fe6755e86826
chee86j <jeffreychee86@gmail.com>
2026-01-07 17:38:00 -0500

    updated JS Project Names

---
+++ JavaScript Practice/Projects/Project1Calculator/script.js
@@ -0,0 +1,37 @@
+// TODO 1: Select DOM Elements
+// - Get the display element
+// - Get all button elements
+
+// TODO 2: Create variables to store:
+// - First number
+// - Operation
+// - Second number
+// - Flag for resetting display
+
+// TODO 3: Add Event Listeners
+// - Add click events to all buttons
+// - Consider using event delegation
+
+// TODO 4: Create handler functions:
+
+// handleNumber(num)
+// - Update display with clicked number
+// - Consider when to reset display
+
+// handleOperator(op)
+// - Store the first number
+// - Store the operation
+// - Prepare for second number input
+
+// handleEquals()
+// - Get the second number
+// - Perform the stored operation
+// - Update display with result
+// - Handle division by zero
+// - Reset calculator state
+
+// handleClear()
+// - Reset all variables
+// - Clear the display
+
+// Add keyboard support
