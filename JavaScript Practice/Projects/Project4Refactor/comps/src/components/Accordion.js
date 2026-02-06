6cf4e83d333a02c4e4c3e77a7addf2f3501bf33a
chee86j <jeffreychee86@gmail.com>
2026-02-06 17:45:00 -0500

    Adding Conditional Rendering to Accordion

--- JavaScript Practice/Projects/Project4Refactor/comps/src/components/Accordion.js
+++ JavaScript Practice/Projects/Project4Refactor/comps/src/components/Accordion.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    // Adding Conditional Rendering
     const renderedItems = items.map((item) => {
+        const isExpanded = index === expandedIndex;
+    
         return (
         <div key={item.id}>
             <div>{item.label}</div>
-            <div>{item.content}</div>
+            {isExpanded && <div>{item.content}</div>}
         </div>
         );
     });
@@ -11,3 +18,11 @@ function Accordion({ items }) {
 }
 
 export default Accordion;
+
+/*
+Our conditional rendering logic in the above code follows this pattern:
+1. Check if the current item is the expanded item that matches the index 
+of the item that was clicked.
+2. If it is, render the content (renderedItems)
+3. If it is not, do not render the content (renderedItems)
+*/
