1cbb55fc6524bc3d3d6b2e6fee942fd6bd478c1f
chee86j <jeffreychee86@gmail.com>
2026-03-19 20:03:33 -0400

    add reusable portal modal with close handlers and body scroll lock

--- JavaScript Practice/Projects/Project6NavBarReactRouter/comps/src/components/Modal.js
+++ JavaScript Practice/Projects/Project6NavBarReactRouter/comps/src/components/Modal.js
@@ -1,13 +1,33 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
-function Modal() {
-    return ReactDOM.createPortal (
-        <div>
-        <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
-        <div className='absolute inset-40 p-10 bg-white'>I'm a modal</div>
+function Modal({ onClose, children, actionBar }) {
+    // Disable scrolling on the body when the modal is open
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden');
+        
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, []);
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 z-50">
+      {/* Clicking the backdrop means the user clicked outside the modal therefore we close it. */}
+      <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+      <div
+        className="fixed inset-40 bg-white p-10">
+        <div className='flex flex-col justify-between h-full'>
+            {children}
+            <div className='flex justify-end'>
+                {actionBar}
+            </div>
+        </div>
+        
+      </div>
     </div>,
     document.querySelector('.modal-container')
-    )
+  );
 }
 
 export default Modal
@@ -17,4 +37,12 @@ export default Modal
     2. The modal should sit on top of everything else so existing content is blocked out.
     3. Render the modal with a portal so it mounts into `.modal-container` in index.html.
     4. Keep modal markup separate from the page layout so stacking and positioning stay predictable.
+
+    Why we need portals
+    - Portals allow us to render a component's JSX outside of its parent component's DOM hierarchy. 
+    - This is useful for modals, tooltips, and other UI elements that need to visually break out of their parent container.
+    - It provides a cleaner layering above the app content and avoids CSS complications that arise from nesting modals inside other components.
+      i.e. z-index/stacking context issues, overflow:hidden on parent containers, etc.
+    - This is the more predictable and safer way to implement modals in React.
+
 */
--- JavaScript Practice/Projects/Project6NavBarReactRouter/comps/src/pages/ModalPage.js
+++ JavaScript Practice/Projects/Project6NavBarReactRouter/comps/src/pages/ModalPage.js
@@ -3,18 +3,77 @@ import Modal from '../components/Modal';
 import Button from '../components/Button';
 
 function ModalPage() {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const handleClick = () => {
-        setShowModal(true);
-    }
+  const handleClick = () => {
+    setShowModal(true);
+  };
 
-    return (
-        <div className='relative'>
-            <Button onClick={handleClick} primary>OpenModal</Button>
-            {showModal && <Modal />}
-        </div>
-    )
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept.</p>
+    </Modal>
+  );
+
+  return (
+    <div>
+      
+      <Button onClick={handleClick} primary>
+        OpenModal
+      </Button>
+      {showModal && modal}
+      
+      <p>
+        Sunt elit sit id veniam ad sit duis sunt do qui minim aliquip. Nulla quis id proident dolor amet laboris proident adipisicing. Nulla ea id irure magna ut nostrud cupidatat. Nisi enim quis aliqua non reprehenderit amet amet anim consectetur aliqua. Laborum elit nulla duis proident est do sit duis commodo eiusmod est officia. Occaecat aliqua commodo est aliqua tempor ex aute est.
+      </p>
+      <p>
+        Lorem ipsum labore non eiusmod eu commodo ea do nulla mollit esse qui ea. Laboris consectetur elit exercitation reprehenderit eiusmod pariatur aliqua est. Ex culpa velit labore sint pariatur id dolor magna amet sint esse nostrud ut mollit. Amet occaecat magna voluptate incididunt non anim ipsum do incididunt culpa. Labore enim ea sint eu.
+      </p>
+      <p>
+        Laboris dolor laboris labore do nisi aute veniam ullamco. Non qui fugiat officia id mollit aliqua sunt deserunt exercitation ad quis officia. Aute occaecat sit mollit ad reprehenderit mollit laborum pariatur ex.
+      </p>
+      <p>
+        Sunt elit sit id veniam ad sit duis sunt do qui minim aliquip. Nulla quis id proident dolor amet laboris proident adipisicing. Nulla ea id irure magna ut nostrud cupidatat. Nisi enim quis aliqua non reprehenderit amet amet anim consectetur aliqua. Laborum elit nulla duis proident est do sit duis commodo eiusmod est officia. Occaecat aliqua commodo est aliqua tempor ex aute est.
+      </p>
+      <p>
+        Lorem ipsum labore non eiusmod eu commodo ea do nulla mollit esse qui ea. Laboris consectetur elit exercitation reprehenderit eiusmod pariatur aliqua est. Ex culpa velit labore sint pariatur id dolor magna amet sint esse nostrud ut mollit. Amet occaecat magna voluptate incididunt non anim ipsum do incididunt culpa. Labore enim ea sint eu.
+      </p>
+      <p>
+        Laboris dolor laboris labore do nisi aute veniam ullamco. Non qui fugiat officia id mollit aliqua sunt deserunt exercitation ad quis officia. Aute occaecat sit mollit ad reprehenderit mollit laborum pariatur ex.
+      </p>
+      <p>
+        Sunt elit sit id veniam ad sit duis sunt do qui minim aliquip. Nulla quis id proident dolor amet laboris proident adipisicing. Nulla ea id irure magna ut nostrud cupidatat. Nisi enim quis aliqua non reprehenderit amet amet anim consectetur aliqua. Laborum elit nulla duis proident est do sit duis commodo eiusmod est officia. Occaecat aliqua commodo est aliqua tempor ex aute est.
+      </p>
+      <p>
+        Lorem ipsum labore non eiusmod eu commodo ea do nulla mollit esse qui ea. Laboris consectetur elit exercitation reprehenderit eiusmod pariatur aliqua est. Ex culpa velit labore sint pariatur id dolor magna amet sint esse nostrud ut mollit. Amet occaecat magna voluptate incididunt non anim ipsum do incididunt culpa. Labore enim ea sint eu.
+      </p>
+      <p>
+        Laboris dolor laboris labore do nisi aute veniam ullamco. Non qui fugiat officia id mollit aliqua sunt deserunt exercitation ad quis officia. Aute occaecat sit mollit ad reprehenderit mollit laborum pariatur ex.
+      </p>
+      <p>
+        Sunt elit sit id veniam ad sit duis sunt do qui minim aliquip. Nulla quis id proident dolor amet laboris proident adipisicing. Nulla ea id irure magna ut nostrud cupidatat. Nisi enim quis aliqua non reprehenderit amet amet anim consectetur aliqua. Laborum elit nulla duis proident est do sit duis commodo eiusmod est officia. Occaecat aliqua commodo est aliqua tempor ex aute est.
+      </p>
+      <p>
+        Lorem ipsum labore non eiusmod eu commodo ea do nulla mollit esse qui ea. Laboris consectetur elit exercitation reprehenderit eiusmod pariatur aliqua est. Ex culpa velit labore sint pariatur id dolor magna amet sint esse nostrud ut mollit. Amet occaecat magna voluptate incididunt non anim ipsum do incididunt culpa. Labore enim ea sint eu.
+      </p>
+      <p>
+        Laboris dolor laboris labore do nisi aute veniam ullamco. Non qui fugiat officia id mollit aliqua sunt deserunt exercitation ad quis officia. Aute occaecat sit mollit ad reprehenderit mollit laborum pariatur ex.
+      </p>
+
+    </div>
+  );
 }
 
 export default ModalPage;
